b22f2f223ab03c936b585ddaa6a7844d0dea1b5d
Kartik <kartx111@gmail.com>
2018-03-20 09:38:05 +0000

    feat: exif parser for files in folder

--- renderer.js
+++ renderer.js
@@ -2,6 +2,9 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 var fs = require('fs');
+const exiftool = require('node-exiftool');
+const ep = new exiftool.ExiftoolProcess();
+
 var submitButton = document.getElementById('submitBtn');
 
 function getFiles (dir, files_){
@@ -18,10 +21,32 @@ function getFiles (dir, files_){
     return files_;
 }
 
+function processFile(file) {
+	const rs = fs.createReadStream(file);
+	return ep.open()
+	.then(
+		function() {
+		    ep.readMetadata(rs, ['-File:all']);
+		};
+	).then(
+		function(res) {
+			console.log(res);
+		};
+	);
+}
+
+function processFiles(files_) {
+	for (var i in files_){
+		console.log(files_[i]);
+        processFile(files_[i]);
+    }
+}
+
 submitBtn.addEventListener('click', function(){
-	console.log('Testing');
 	var folderName = document.getElementById('myFile').value;
-	// folderName = '/home/ubuntu/workspace/kartik';
-	console.log(getFiles(folderName));
+	folderName = '/home/ubuntu/workspace/kartik';
+	var fileList = getFiles(folderName);
+	// console.log(fileList);
+	processFiles(fileList);
 });
 
