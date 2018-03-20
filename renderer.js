a44acde1c0dc1930b4dda7f6650e36cbe29d5452
Kartik <kartx111@gmail.com>
2018-03-20 07:59:33 +0000

    feat: Electron Boilerplate for Directory Processing

---
+++ renderer.js
@@ -0,0 +1,7 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+var submitButton = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', function(){
+	console.log('Testing');
+});
