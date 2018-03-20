4992a63860fd18479382d5df9e2fd097a098a995
Kartik <kartx111@gmail.com>
2018-03-20 08:20:14 +0000

    feat: read folder contents recursively

--- renderer.js
+++ renderer.js
@@ -1,7 +1,27 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var fs = require('fs');
 var submitButton = document.getElementById('submitBtn');
+
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+
 submitBtn.addEventListener('click', function(){
 	console.log('Testing');
+	var folderName = document.getElementById('myFile').value;
+	// folderName = '/home/ubuntu/workspace/kartik';
+	console.log(getFiles(folderName));
 });
+
